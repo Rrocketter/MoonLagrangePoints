c1aff9cb2a783eeae3c67dcd79ecd76b9cf3bc38
rGupta711 <116116756+rGupta711@users.noreply.github.com>
2023-12-13 12:52:57 -0500

    fixed l1 simulation

--- l1animation.py
+++ l1animation.py
@@ -10,59 +10,61 @@ R_earth_moon = 3.84e8
 
 # Function to calculate Lagrange point L1
 def lagrange_point_l1(moonlet_mass, distance_earth_moon):
-    r1 = (M_moon * R_earth_moon) / (M_earth + M_moon)
-    r_l1 = distance_earth_moon * (M_earth / (3 * M_earth + moonlet_mass))**(1/3)
-    return r1 + r_l1
+    r_l1 = distance_earth_moon * (M_earth / (3 * (M_earth + moonlet_mass)))**(1/3)
+    return r_l1
 
 # Function to calculate orbital parameters at Lagrange point L1
-def calculate_orbital_parameters(moonlet_mass, distance_earth_moon, frame):
+def calculate_orbital_parameters(moonlet_mass, distance_earth_moon):
     r_l1 = lagrange_point_l1(moonlet_mass, distance_earth_moon)
     omega = np.sqrt(G * (M_earth + moonlet_mass) / r_l1**3)
     orbital_period = 2 * np.pi / omega
     orbital_speed = omega * r_l1
+    return omega, orbital_period, orbital_speed, r_l1
 
-    # Calculate position of Moonlet at the given frame
-    moonlet_x = r_l1 * np.cos(omega * frame)
-    moonlet_y = r_l1 * np.sin(omega * frame)
+# Example values
+moonlet_mass = 1e15
 
-    return omega, orbital_period, orbital_speed, r_l1, moonlet_x, moonlet_y
+# Calculate orbital parameters at Lagrange point L1
+angular_velocity, period, speed, r_l1 = calculate_orbital_parameters(moonlet_mass, R_earth_moon)
+
+# Plotting
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Plot the Earth (scaled down for better visualization)
+earth_circle = plt.Circle((0, 0), R_earth_moon / (5 * (1 + M_moon/M_earth)), color='blue', label='Earth')
+ax.add_patch(earth_circle)
+
+# Plot the Moon
+moon_circle = plt.Circle((R_earth_moon, 0), R_earth_moon / (1 + M_earth/M_moon), color='gray', label='Moon')
+ax.add_patch(moon_circle)
+
+# Plot the Lagrange point L1 (slightly bigger for better visibility)
+l1_point = plt.Circle((r_l1, 0), 2e6, color='red', label='L1 Point')  # Increased the size of L1 Point
+ax.add_patch(l1_point)
+
+# Plot the moonlet orbiting around L1 (made smaller for better visual distinction)
+moonlet = plt.Circle((r_l1, 0), 5e5, color='green', label='Moonlet')  # Adjusted the size of Moonlet
+ax.add_patch(moonlet)
+
+ax.set_xlim(-1.5 * R_earth_moon, 1.5 * R_earth_moon)
+ax.set_ylim(-1.5 * R_earth_moon, 1.5 * R_earth_moon)
+ax.set_aspect('equal', 'box')
+plt.title("Earth, Moon, Lagrange Point L1, and Moonlet")
+plt.legend()
 
 # Animation function
 def update(frame):
-    ax.clear()
+    angle = angular_velocity * frame
 
-    # Calculate orbital parameters at Lagrange point L1
-    moonlet_mass = 1e15 + 1e14 * np.sin(0.02 * frame)  # Varying moonlet mass for animation
-    distance_earth_moon = R_earth_moon
+    # Update the position of the Moon
+    moon_circle.set_center((R_earth_moon * np.cos(angle), R_earth_moon * np.sin(angle)))
 
-    omega, period, speed, r_l1, moonlet_x, moonlet_y = calculate_orbital_parameters(moonlet_mass, distance_earth_moon, frame)
+    # Update the position of Lagrange point L1
+    l1_point.set_center((r_l1 * np.cos(angle), r_l1 * np.sin(angle)))
 
-    # Plot the Earth (scaled down for better visualization)
-    earth_circle = plt.Circle((0, 0), R_earth_moon / (5 * (1 + M_moon/M_earth)), color='blue', label='Earth')
-    ax.add_patch(earth_circle)
+    # Update the position of the Moonlet
+    moonlet.set_center((r_l1 * np.cos(angle), r_l1 * np.sin(angle)))
 
-    # Plot the Moon
-    moon_circle = plt.Circle((R_earth_moon, 0), R_earth_moon / (1 + M_earth/M_moon), color='gray', label='Moon')
-    ax.add_patch(moon_circle)
-
-    # Plot the Lagrange point L1 (size dynamically changing)
-    l1_point = plt.Circle((r_l1, 0), 2e6 + 5e5 * np.sin(0.02 * frame), color='red', label='L1 Point')
-    ax.add_patch(l1_point)
-
-    # Plot the moonlet orbiting around L1 (size dynamically changing)
-    moonlet = plt.Circle((moonlet_x, moonlet_y), 5e5, color='green', label='Moonlet')
-    ax.add_patch(moonlet)
-
-    ax.set_xlim(-1.5 * R_earth_moon, 1.5 * R_earth_moon)
-    ax.set_ylim(-1.5 * R_earth_moon, 1.5 * R_earth_moon)
-    ax.set_aspect('equal', 'box')
-    plt.title(f"Frame {frame}")
-    plt.legend()
-
-# Set up the plot
-fig, ax = plt.subplots()
-
-# Animate the plot
-frames = np.arange(0, 400, 1)
-animation = FuncAnimation(fig, update, frames=frames, interval=50, blit=False)
+# Animate the motion
+animation = FuncAnimation(fig, update, frames=np.arange(0, period, 1000), interval=50)
 plt.show()
