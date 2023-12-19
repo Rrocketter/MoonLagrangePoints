8579568faa3ea7f348d064975f453a8d1bc93278
Rrocketter <rahul.rocket711@gmail.com>
2023-12-19 18:46:30 -0500

    L1 moonlet mass & distance altered

--- l1.py
+++ l1.py
@@ -23,15 +23,34 @@ def calculate_orbital_parameters(moonlet_mass, distance_earth_moon):
     return omega, orbital_period, orbital_speed
 
 # Example values
-moonlet_mass = 1e18  # Example moonlet mass in kg
+moonlet_mass_values = [1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 5e18, 1e19, 5e19]  # Example moonlet mass in kg
 
-# Calculate orbital parameters at Lagrange point L1
-angular_velocity, period, speed = calculate_orbital_parameters(moonlet_mass, R_earth_moon)
+for moonlet_mass in moonlet_mass_values:
+    # Calculate orbital parameters at Lagrange point L1
+    angular_velocity, period, speed = calculate_orbital_parameters(moonlet_mass, R_earth_moon)
+    l1 = lagrange_point_l1(moonlet_mass, R_earth_moon)
 
-l1 = lagrange_point_l1(moonlet_mass, R_earth_moon)
-print(l1)
+    # Print the results to the console
+    print(f"\nMoonlet Mass: {moonlet_mass} kg")
+    print(f"Lagrange Point L1: {l1} meters")
+    print(f"Angular Velocity (w) at Lagrange Point L1: {angular_velocity} rad/s")
+    print(f"Orbital Period at Lagrange Point L1: {period} seconds")
+    print(f"Orbital Speed at Lagrange Point L1: {speed} m/s")
 
-# Print the results to the console
-print(f"Angular Velocity (w) at Lagrange Point L1: {angular_velocity} rad/s")
-print(f"Orbital Period at Lagrange Point L1: {period} seconds")
-print(f"Orbital Speed at Lagrange Point L1: {speed} m/s")
+print("----------------------------------------")
+print("----------------------------------------")
+print("----------------------------------------")
+print("----------------------------------------")
+
+moonlet_mass_1 = 1e18
+for distance_earth_moon in np.arange(3.84e8, 4.84e8, 1e6):
+    # Calculate orbital parameters at Lagrange point L1
+    angular_velocity, period, speed = calculate_orbital_parameters(moonlet_mass_1, distance_earth_moon)
+    l1 = lagrange_point_l1(moonlet_mass_1, distance_earth_moon)
+
+    # Print the results to the console
+    print(f"\nDistance Between Earth and Moon: {distance_earth_moon} meters")
+    print(f"Lagrange Point L1: {l1} meters")
+    print(f"Angular Velocity (w) at Lagrange Point L1: {angular_velocity} rad/s")
+    print(f"Orbital Period at Lagrange Point L1: {period} seconds")
+    print(f"Orbital Speed at Lagrange Point L1: {speed} m/s")
